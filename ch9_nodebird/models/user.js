d312d974d16be8ee296597b70ecb46874fe8f722
jang-hs <retrorca@gmail.com>
2022-05-24 23:51:52 +0900

    20220524 ch9 add db setting

--- ch9_nodebird/models/user.js
+++ ch9_nodebird/models/user.js
@@ -19,6 +19,7 @@ module.exports = class User extends Sequelize.Model {
       provider: {
         type: Sequelize.STRING(10),
         allowNull: false,
+        // 기본값 지정.
         defaultValue: 'local',
       },
       snsId: {
@@ -26,6 +27,7 @@ module.exports = class User extends Sequelize.Model {
         allowNull: true,
       },
     }, {
+      //timestamps, paranoid true -> createAt, updatedAt, deleteAt 컬럼도 생성됨.
       sequelize,
       timestamps: true,
       underscored: false,
@@ -38,16 +40,19 @@ module.exports = class User extends Sequelize.Model {
   }
 
   static associate(db) {
+    // user 와 post 는 1:N 관계
     db.User.hasMany(db.Post);
+    // 같은 모델 끼리도 N:M 관계를 가질 수 있음. 사용자 한 명이 여러 팔로워를 갖거나, 한 사람이 여러명을 팔로워하는 경우
+    // 팔로워(Followers)를 찾으려면 먼저 팔로잉하는 사람의 Id(followingId)를 찾아야 함.
     db.User.belongsToMany(db.User, {
       foreignKey: 'followingId',
       as: 'Followers',
-      through: 'Follow',
+      through: 'Follow', // 생성 모델 명
     });
     db.User.belongsToMany(db.User, {
       foreignKey: 'followerId',
       as: 'Followings',
-      through: 'Follow',
+      through: 'Follow',  // 생성 모델 명
     });
   }
 };
