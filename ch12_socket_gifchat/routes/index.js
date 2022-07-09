0408d1ec621f853bde21b8237b254110dec6eec3
jang-hs <retrorca@gmail.com>
2022-07-09 14:22:08 +0900

    220709 gif채팅 구현

--- ch12_socket_gifchat/routes/index.js
+++ ch12_socket_gifchat/routes/index.js
@@ -1,9 +1,86 @@
 const express = require('express');
 
+const Room = require('../schemas/room');
+const Chat = require('../schemas/chat');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index');
+router.get('/', async (req, res, next) => {
+  try {
+    const rooms = await Room.find({});
+    res.render('main', { rooms, title: 'GIF 채팅방' });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/room', (req, res) => {
+  res.render('room', { title: 'GIF 채팅방 생성' });
+});
+
+// 채팅방 생성
+router.post('/room', async (req, res, next) => {
+  try {
+    const newRoom = await Room.create({
+      title: req.body.title,
+      max: req.body.max,
+      owner: req.session.color,
+      password: req.body.password,
+    });
+    // app.set('io', io)로 저장한 객체 가져옴
+    const io = req.app.get('io');
+    // /room 네임스페이스에 연결한 모든 클라이언트에게 데이터를 보냄.
+    io.of('/room').emit('newRoom', newRoom);
+    res.redirect(`/room/${newRoom._id}?password=${req.body.password}`);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+// 채팅방 렌더링
+router.get('/room/:id', async (req, res, next) => {
+  try {
+    const room = await Room.findOne({ _id: req.params.id });
+    const io = req.app.get('io');
+    // 방 존재여부
+    if (!room) {
+      return res.redirect('/?error=존재하지 않는 방입니다.');
+    }
+    // 비밀번호 맞는지 확인
+    if (room.password && room.password !== req.query.password) {
+      return res.redirect('/?error=비밀번호가 틀렸습니다.');
+    }
+    const { rooms } = io.of('/chat').adapter; //방 몽록
+    // 허용인원 확인
+    if (rooms && rooms[req.params.id] && room.max <= rooms[req.params.id].length) {
+      return res.redirect('/?error=허용 인원이 초과하였습니다.');
+    }
+    return res.render('chat', {
+      room,
+      title: room.title,
+      chats: [],
+      user: req.session.color,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+router.delete('/room/:id', async (req, res, next) => {
+  try {
+    await Room.remove({ _id: req.params.id });
+    await Chat.remove({ room: req.params.id });
+    res.send('ok');
+    setTimeout(() => {
+      // /room 네임스페이스에 방 삭제되었음을 알림.
+      req.app.get('io').of('/room').emit('removeRoom', req.params.id);
+    }, 2000);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
--- ch12_socket_gifchat/socket.js
+++ ch12_socket_gifchat/socket.js
@@ -1,12 +1,20 @@
 const SocketIO = require('socket.io');
+const axios = require('axios');
+const cookieParser = require('cookie-parser');
+const cookie = require('cookie-signature');
 
-module.exports = (server, app) => {
+module.exports = (server, app, sessionMiddleware) => {
   const io = SocketIO(server, { path: '/socket.io' });
   // io 객체를 쓸 수 있게 저장해 둠. (req.app.get('io')로 접근 가능)
   app.set('io', io);
   // of 를 사용해서 socket.io에 네임스페이스를 부여함. 같은 네임스페이스끼리만 데이터를 전달함.
   const room = io.of('/room'); // 채팅방 생성 및 삭제에 관한 정보 전달
   const chat = io.of('/chat'); // 채팅 메시지 전달
+  // 세션 미들웨어 장착. 웹 소켓 연결시마다 실행. 요청, 응답, next 함수
+  io.use((socket, next) => {
+    cookieParser(process.env.COOKIE_SECRET)(socket.request, socket.request.res, next);
+    sessionMiddleware(socket.request, socket.request.res, next);
+  })
   // room에 이벤트 리스너 붙임.
   room.on('connection', (socket) => {
     console.log('room 네임스페이스에 접속');
@@ -25,10 +33,39 @@ module.exports = (server, app) => {
       .replace(/\?.+/, '');
     // 네임스페이스 접속시 socket.join 메서드
     socket.join(roomId);
+    // 특정 방에 데이터를 보낼 수 있음.
+    socket.to(roomId).emit('join', {
+      user: 'system',
+      chat: `${req.session.color}님이 입장하셨습니다.`
+    })
     // 접속 해제시 leave 메서드
     socket.on('disconnect', () => {
       console.log('chat 네임스페이스 접속 해제');
       socket.leave(roomId);
+      const currentRoom = socket.adapter.rooms[roomId];
+      const userCount = currentRoom ? currentRoom.length : 0;
+      // 접속자가 0명이면 방 삭제
+      if (userCount === 0) {
+        const signedCookie = req.signedCookies['connect.sid'];
+        const connectSID = cookie.sign(signedCookie, process.env.COOKIE_SECRET);
+        // socket에서 axios 요청을 보낼 때 요청자의 정보가 없기때문에 요청 헤더에 세션 쿠키를 직접 넣음.
+        axios.delete(`http://localhost:8005/room/${roomId}`, {
+          headers: {
+            Cookie: `connect.sid=s%3A${connectSID}`,
+          }
+        })
+          .then(() => {
+            console.log('방 제거 요청 성공')
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      } else {
+        socket.to(roomId).emit('exit', {
+          user: 'system',
+          chat: `${req.session.color}님이 퇴장하셨습니다.`
+        });
+      }
     });
   });
 };
